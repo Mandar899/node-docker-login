c6302cb9232e2f510ab2fa5fe184c66690878786
Mandar Manvi <mandar@damacloid.com>
2022-03-14 14:31:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+// Read dotenv files
+require("dotenv").config();
+
+const userRouter = require("./routes/users");
+
+// Parse json data
+app.use(express.json());
+
+app.use("/users", userRouter);
+
+// mongoose
+const mongoose = require("mongoose");
+
+// mongodb connect
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// PORT
+const PORT = 8080;
+
+// Listen to PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
